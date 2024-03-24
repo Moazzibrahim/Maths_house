ff4418437797992ffdc5630cd1e8e9419354f516
Moaz Ibrahim <98399366+wezza99@users.noreply.github.com>
2024-03-24 17:13:56 +0200

    first commit responsive2

--- lib/View/screens/registered_home_screen.dart
+++ lib/View/screens/registered_home_screen.dart
@@ -12,40 +12,60 @@ class RegisteredHomeScreen extends StatelessWidget {
           padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
-              const Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage(
-                        'assets/images/moaz.jpeg'), // Your image path here
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    'Welcome Moaz', // Change 'John' to the desired name
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+              const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundImage: AssetImage(
+                          'assets/images/moaz.jpeg'), // Your image path here
                     ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Welcome Moaz', // Change 'John' to the desired name
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              GridView(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                ),
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                children: [
+                  GridContainer(
+                    text: 'Courses',
+                    color: Colors.red[200],
+                    styleColor: Colors.redAccent[700],
+                  ),
+                  GridContainer(
+                    text: 'Exams',
+                    color: Colors.blue[200],
+                    styleColor: Colors.blueAccent[700],
+                  ),
+                  GridContainer(
+                    text: 'Quizes',
+                    color: Colors.purple[200],
+                    styleColor: Colors.purpleAccent[700],
+                  ),
+                  GridContainer(
+                    text: 'Diagnostic Exams',
+                    color: Colors.green[200],
+                    styleColor: Colors.greenAccent[700],
                   ),
                 ],
               ),
-              const SizedBox(height: 10,),
-              GridView(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 20,
-                    crossAxisSpacing: 20,
-                    ),
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    children: [
-                      GridContainer(text: 'Courses', color: Colors.red[200], styleColor: Colors.redAccent[700],),
-                      GridContainer(text: 'Exams', color: Colors.blue[200], styleColor: Colors.blueAccent[700],),
-                      GridContainer(text: 'Quizes', color: Colors.purple[200], styleColor: Colors.purpleAccent[700],),
-                      GridContainer(text: 'Diagnostic Exams', color: Colors.green[200], styleColor: Colors.greenAccent[700],),
-                    ],
-              ),
-              
             ],
           ),
         ),
