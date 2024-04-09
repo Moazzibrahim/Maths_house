47f8e3365d81d3118ccd7a658ca9648adf98347b
Moaz Ibrahim <98399366+wezza99@users.noreply.github.com>
2024-04-09 16:37:34 +0200

    sbaho diagnostic

--- lib/View/screens/registered_home_screen.dart
+++ lib/View/screens/registered_home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_application_1/View/screens/Diagnostic_exams.dart/filter_screen.dart';
 import 'package:flutter_application_1/View/screens/courses_screen.dart';
 import 'package:flutter_application_1/View/screens/exam-view/exam_filteration_screen.dart';
