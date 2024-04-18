7c7fa2057f4f8f5dc6550aeeaa515a0e849b7616
Moaz Ibrahim <98399366+wezza99@users.noreply.github.com>
2024-04-18 12:57:09 +0200

    sbaho diagnosticccssss

--- lib/View/screens/Diagnostic_exams/diagnostic_result_screen.dart
+++ lib/View/screens/Diagnostic_exams/diagnostic_result_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/View/screens/registered_home_screen.dart';
+import 'package:flutter_application_1/constants/colors.dart';
 import 'package:flutter_application_1/constants/widgets.dart';
 
 class DiagnosticResultScreen extends StatefulWidget {
@@ -10,6 +11,8 @@ class DiagnosticResultScreen extends StatefulWidget {
 }
 
 class _DiagnosticResultScreenState extends State<DiagnosticResultScreen> {
+  bool showRecommendation = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +20,201 @@ class _DiagnosticResultScreenState extends State<DiagnosticResultScreen> {
       body: Column(
         children: [
           Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: gridHomeColor,
+            ),
             child: const Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Quizes"),
-                Text("ADf")
+                Text(
+                  "Quizes",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                ),
+                Text(
+                  "ADf",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                )
               ],
             ),
-          )
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          // Other containers representing quiz information
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: gridHomeColor,
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Score",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                ),
+                Text(
+                  "5",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: gridHomeColor,
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Total questions",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                ),
+                Text(
+                  "10",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: gridHomeColor,
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "correct questions",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                ),
+                Text(
+                  "6",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: gridHomeColor,
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Wrong questions",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                ),
+                Text(
+                  "0",
+                  style: TextStyle(fontSize: 10, color: Colors.black),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: faceBookColor),
+            onPressed: () {
+              setState(() {
+                showRecommendation = !showRecommendation;
+              });
+            },
+            child: const Text(
+              "Recommended",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          if (showRecommendation) ...[
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Chapter 1",
+              style: TextStyle(fontSize: 16, color: Colors.black),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(color: faceBookColor)),
+                  onPressed: () {
+                    const ScaffoldMessenger(
+                        child: SnackBar(
+                            backgroundColor: Colors.black,
+                            content: Text(
+                              "Done",
+                              style: TextStyle(color: Colors.white),
+                            )));
+                    // Action for the first elevated button
+                  },
+                  child: const Text(
+                    "Buy",
+                    style: TextStyle(
+                      color: faceBookColor,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: faceBookColor),
+                  onPressed: () {
+                    // Action for the second elevated button
+                    const ScaffoldMessenger(
+                        child: SnackBar(
+                            backgroundColor: Colors.black,
+                            content: Text(
+                              "Done",
+                              style: TextStyle(color: Colors.white),
+                            )));
+                  },
+                  child: const Text(
+                    "Buy All",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: faceBookColor),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const RegisteredHomeScreen(),
+                    ),);
+              },
+              child: const Text(
+                "Home",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ],
       ),
     );
