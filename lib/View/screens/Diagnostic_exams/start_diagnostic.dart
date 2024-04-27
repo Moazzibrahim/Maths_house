8ef45bd0c3a7047400e0e7859b1d73c91d35604e
Moaz Ibrahim <98399366+wezza99@users.noreply.github.com>
2024-04-27 09:27:47 +0300

    sbaho exam recommends

--- lib/View/screens/Diagnostic_exams/start_diagnostic.dart
+++ lib/View/screens/Diagnostic_exams/start_diagnostic.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: avoid_print, unused_local_variable
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Model/login_model.dart';
 import 'package:flutter_application_1/View/screens/Diagnostic_exams/diagnostic_result_screen.dart';
 import 'package:flutter_application_1/constants/colors.dart';
 import 'package:flutter_application_1/controller/Timer_provider.dart';
 import 'package:flutter_application_1/controller/diagnostic/diagnostic_exam_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class StartDiagnostic extends StatelessWidget {
   // ignore: use_key_in_widget_constructors
@@ -93,7 +96,7 @@ class _DiagnosticBodyState extends State<DiagnosticBody> {
           return SimpleDialog(
             title: const Text('Go to Question'),
             children: List.generate(
-              questionData.length,
+              allDiagnostics.length,
               (index) => SimpleDialogOption(
                 onPressed: () {
                   setState(() {
@@ -107,6 +110,56 @@ class _DiagnosticBodyState extends State<DiagnosticBody> {
           );
         },
       );
+    }
+
+    void postDiagExamResults(int correctAnswerCount, Duration? elapsedTime,
+        List<int> wrongQuestionIds) async {
+      const url =
+          'https://login.mathshouse.net/api/MobileStudent/ApiMyCourses/stu_dia_exam_grade';
+      final startExamProvider =
+          Provider.of<DiagExamProvider>(context, listen: false);
+      final examId = startExamProvider.exid;
+
+      // Check if elapsedTime is null, if so, initialize it to Duration.zero
+      elapsedTime ??= Duration.zero;
+      // Calculate elapsed time in minutes and seconds as a combined string
+      final String elapsed =
+          '${elapsedTime.inMinutes}:${(elapsedTime.inSeconds % 60).toString().padLeft(2, '0')}';
+      final int elapsedMinutes = elapsedTime.inMinutes;
+      final int elapsedSeconds = elapsedTime.inSeconds % 60;
+
+      final Map<String, dynamic> postData = {
+        'exam_id': examId,
+        'right_question': correctAnswerCount,
+        'timer': elapsedMinutes,
+        'mistakes': wrongQuestionIds,
+      };
+
+      final tokenProvider = Provider.of<TokenModel>(context, listen: false);
+      final token = tokenProvider.token; // Replace with your auth token
+
+      try {
+        final response = await http.post(
+          Uri.parse(url),
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+          body: json.encode(postData),
+        );
+
+        if (response.statusCode == 200) {
+          print(' Diag Exam results posted successfully.');
+          print("exam id: $examId");
+        } else {
+          print('Failed to post exam results: ${response.statusCode}');
+          // Print response body for more details
+          print('Response body: ${response.body}');
+        }
+      } catch (e) {
+        print('Error posting exam results: $e');
+      }
     }
 
     void submitAnswers() {
@@ -250,6 +303,15 @@ class _DiagnosticBodyState extends State<DiagnosticBody> {
               child: const Text(
                 'Previous',
                 style: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                navigateToQuestion(context);
+              },
+              child: const Text(
+                'Go to Question',
+                style: TextStyle(color: faceBookColor),
               ),
             ),
             ElevatedButton(
