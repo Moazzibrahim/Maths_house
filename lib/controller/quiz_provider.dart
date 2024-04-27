17b546bf75b8369cd46b3ca5ac022e4fd64896b5
yousseftamer2003 <yousseftamer2003.yt@gmail.com>
2024-04-27 09:53:13 +0300

    quizzes api modeling and ui is in progress

---
+++ lib/controller/quiz_provider.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Model/login_model.dart';
+import 'package:flutter_application_1/Model/quizzes_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+
+class QuizzesProvider with ChangeNotifier {
+  List<QuizzesModel> allQuizzesModel= [];
+  Future<void> getQuizzesData(BuildContext context, int id) async {
+    try{
+  final tokenProvider = Provider.of<TokenModel>(context, listen: false);
+    final token = tokenProvider.token;
+    final response = await http.get(
+      Uri.parse(
+          'https://login.mathshouse.net/api/MobileStudent/ApiMyCourses/stu_quiz/$id'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> responseData = jsonDecode(response.body);
+      QuizzesModelList quizzesModelList =
+          QuizzesModelList.fromJson(responseData);
+      List<QuizzesModel> ql = quizzesModelList.quizzesModelList
+          .map((e) => QuizzesModel.fromJson(e))
+          .toList();
+          allQuizzesModel = ql;
+          notifyListeners();
+    }else{
+      log('error, Staatuscode: ${response.statusCode}');
+    }
+  }catch(e){
+    log('error: $e');
+  }
+  }
+}
