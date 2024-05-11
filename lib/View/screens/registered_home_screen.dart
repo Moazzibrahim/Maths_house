19d47a8ff354a5bd66f1e3d854134f5bc731ff43
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-05-11 14:27:47 +0300

    h

--- lib/View/screens/registered_home_screen.dart
+++ lib/View/screens/registered_home_screen.dart
@@ -1,48 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/View/screens/Diagnostic_exams/filter_screen.dart';
 import 'package:flutter_application_1/View/screens/courses/courses_screen.dart';
 import 'package:flutter_application_1/View/screens/exam-view/exam_filteration_screen.dart';
-import 'package:flutter_application_1/View/screens/history_screens/history_screen.dart';
 import 'package:flutter_application_1/View/screens/live_screen.dart';
 import 'package:flutter_application_1/View/screens/questions_filter_screen.dart';
+import 'package:flutter_application_1/View/screens/Diagnostic_exams/filter_screen.dart';
+import 'package:flutter_application_1/View/screens/history_screens/history_screen.dart';
 import 'package:flutter_application_1/View/widgets/grid_container.dart';
 import 'package:flutter_application_1/constants/colors.dart';
+import 'package:flutter_application_1/controller/profile/profile_provider.dart';
+import 'package:provider/provider.dart';
 
 class RegisteredHomeScreen extends StatelessWidget {
-  const RegisteredHomeScreen({super.key});
+  const RegisteredHomeScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    // Initialize ProfileProvider and fetch user data
+    final profileProvider = Provider.of<ProfileProvider>(context);
+    profileProvider.getprofileData(context);
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage('assets/images/logo.jpg'),
-                  ),
-                ],
-              ),
-              const Row(
-                children: [
-                  CircleAvatar(
-                    radius: 25,
-                    backgroundImage: AssetImage('assets/images/moaz.jpeg'),
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    'Welcome student',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+              Consumer<ProfileProvider>(
+                builder: (context, profileProvider, _) {
+                  final userData = profileProvider.userData;
+                  if (userData == null) {
+                    return const CircularProgressIndicator(); // Show loading indicator until user data is fetched
+                  } else {
+                    return Column(
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundImage:
+                                  AssetImage('assets/images/logo.jpg'),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 25,
+                              // Use the user's image here
+                              backgroundImage: NetworkImage(userData.image),
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              'Welcome ${userData.fName}',
+                              style: const TextStyle(
+                                color: faceBookColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        )
+                      ],
+                    );
+                  }
+                },
               ),
               GridView(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
