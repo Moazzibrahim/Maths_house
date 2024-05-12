6134e1401fba6641fa38e5be925c9953bf496d2a
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-05-12 12:14:41 +0300

    hhh

--- lib/View/screens/wallet_recharge.dart
+++ lib/View/screens/wallet_recharge.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/View/screens/checkout/order_details_screen.dart';
 import 'package:flutter_application_1/View/screens/registered_home_screen.dart';
 import 'package:flutter_application_1/constants/colors.dart';
 import 'package:flutter_application_1/constants/widgets.dart';
+import 'package:flutter_application_1/controller/payment_method_provider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class WalletRechargeScreen extends StatefulWidget {
   const WalletRechargeScreen({super.key});
@@ -17,6 +20,7 @@ class WalletRechargeScreen extends StatefulWidget {
 class _WalletRechargeScreenState extends State<WalletRechargeScreen> {
   String? _selectedOption;
   File? _image;
+  TextEditingController _textFieldController = TextEditingController();
 
   Future<void> _uploadFromGallery() async {
     final picker = ImagePicker();
@@ -41,230 +45,217 @@ class _WalletRechargeScreenState extends State<WalletRechargeScreen> {
   }
 
   @override
+  void initState() {
+    Provider.of<PaymentProvider>(context, listen: false)
+        .fetchPaymentMethods(context)
+        .catchError((e) {
+      print(e);
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, 'Wallet Recharge'),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Choose payment method',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(height: 20),
-              ListTile(
-                title: const Text(
-                  'visa',
+    return Consumer<PaymentProvider>(builder: (context, paymentProvider, _) {
+      return Scaffold(
+        appBar: buildAppBar(context, 'Payment'),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Choose payment method',
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: 20,
                   ),
                 ),
-                leading: Radio<String>(
-                  activeColor: faceBookColor,
-                  value: 'visa',
-                  groupValue: _selectedOption,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedOption = value;
-                    });
+                const SizedBox(height: 20),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: paymentProvider.allPaymentMethods.length,
+                  itemBuilder: (context, index) {
+                    final paymentMethod =
+                        paymentProvider.allPaymentMethods[index];
+                    return ListTile(
+                      title: Text(
+                        paymentMethod.payment,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      leading: Radio<String>(
+                        activeColor: faceBookColor,
+                        value: paymentMethod.payment,
+                        groupValue: _selectedOption,
+                        onChanged: (String? value) {
+                          setState(() {
+                            _selectedOption = value;
+                          });
+                        },
+                      ),
+                      tileColor: _selectedOption == paymentMethod.payment
+                          ? gridHomeColor
+                          : null,
+                      onTap: () {
+                        setState(() {
+                          _selectedOption = paymentMethod.payment;
+                        });
+                      },
+                    );
                   },
                 ),
-                tileColor: _selectedOption == 'visa' ? gridHomeColor : null,
-                onTap: () {
-                  setState(() {
-                    _selectedOption = 'visa';
-                  });
-                },
-              ),
-              ListTile(
-                title: const Text('Binance'),
-                leading: Radio<String>(
-                  activeColor: faceBookColor,
-                  value: 'Binance',
-                  groupValue: _selectedOption,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedOption = value;
-                    });
-                  },
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _textFieldController,
+                  decoration: const InputDecoration(
+                    labelText: 'Enter your paid amount',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-                tileColor: _selectedOption == 'Binance' ? gridHomeColor : null,
-                onTap: () {
-                  setState(() {
-                    _selectedOption = 'Binance';
-                  });
-                },
-              ),
-              ListTile(
-                title: const Text('Skrill'),
-                leading: Radio<String>(
-                  activeColor: faceBookColor,
-                  value: 'Skrill',
-                  groupValue: _selectedOption,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedOption = value;
-                    });
-                  },
-                ),
-                tileColor: _selectedOption == 'Skrill' ? gridHomeColor : null,
-                onTap: () {
-                  setState(() {
-                    _selectedOption = 'Skrill';
-                  });
-                },
-              ),
-              ListTile(
-                title: Row(
-                  children: [
-                    Image.asset('assets/images/vodafone_icon 1.png'),
-                    const SizedBox(
-                      width: 5,
+                const SizedBox(height: 20),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    _image != null
+                        ? Image.file(
+                            _image!,
+                            width: 200,
+                            height: 200,
+                          )
+                        : Text(
+                            'Upload receipt',
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                              color: faceBookColor,
+                            ),
+                          ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: _uploadFromGallery,
+                          child: const Text(
+                            'Upload from Gallery',
+                            style: TextStyle(color: faceBookColor),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: _takePhoto,
+                          child: const Text(
+                            'Take Photo',
+                            style: TextStyle(color: faceBookColor),
+                          ),
+                        ),
+                      ],
                     ),
-                    const Text('Vodafone Cash')
                   ],
                 ),
-                leading: Radio<String>(
-                  activeColor: faceBookColor,
-                  value: 'Vodafone Cash',
-                  groupValue: _selectedOption,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedOption = value;
-                    });
-                  },
-                ),
-                tileColor:
-                    _selectedOption == 'Vodafone Cash' ? gridHomeColor : null,
-                onTap: () {
-                  setState(() {
-                    _selectedOption = 'Vodafone Cash';
-                  });
-                },
-              ),
-              const SizedBox(height: 20),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  _image != null
-                      ? Image.file(
-                          _image!,
-                          width: 200,
-                          height: 200,
-                        )
-                      : Text(
-                          'Upload receipt',
-                          style: TextStyle(
-                            fontSize: 20.sp,
-                            color: faceBookColor,
-                          ),
-                        ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        style: const ButtonStyle(),
-                        onPressed: _uploadFromGallery,
-                        child: const Text(
-                          'Upload from Gallery',
-                          style: TextStyle(color: faceBookColor),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: _takePhoto,
-                        child: const Text(
-                          'Take Photo',
-                          style: TextStyle(color: faceBookColor),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Show a dialog
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Request Pending'),
-                        content: const Text('The request is pending.'),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context); // Close the dialog
-                            },
-                            child: const Text('OK'),
-                          ),
-                        ],
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_selectedOption != null && _image != null) {
+                      //submitPayment(_selectedOption!, _image!);
+                      // Show an alert dialog with "Successful Payment" text
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return const AlertDialog(
+                            // title: Text("Payment Successful"),
+                            content: Text("successful payment"),
+                          );
+                        },
                       );
-                    },
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: faceBookColor,
-                  padding: EdgeInsets.symmetric(
-                    vertical: 12.h,
-                    horizontal: 130.w,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.w),
-                  ),
-                ),
-                child: Text(
-                  'submit',
-                  style: TextStyle(
-                    fontSize: 16.sp,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: ScreenUtil().setHeight(
-                        120)), // Adjust the top padding based on screen height
-                child: Center(
-                  child: Container(
-                    width: ScreenUtil().setWidth(
-                        200), // Adjust the width based on screen width
-                    decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.circular(ScreenUtil().setWidth(10)),
-                      color: gridHomeColor,
-                    ),
-                    child: TextButton.icon(
-                      onPressed: () {
+                      // Delay navigation to the order details screen by 2 seconds
+                      Future.delayed(const Duration(seconds: 2), () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => const RegisteredHomeScreen()),
+                              builder: (context) => const OrderDetails()),
                         );
-                      },
-                      icon: const Icon(
-                        Icons.home,
-                        color: faceBookColor,
+                      });
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text("Error"),
+                            content: const Text(
+                                "Please select a payment method and upload a receipt."),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("OK"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: faceBookColor,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 12.h,
+                      horizontal: 130.w,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.w),
+                    ),
+                  ),
+                  child: Text(
+                    'submit',
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: ScreenUtil().setHeight(
+                          120)), // Adjust the top padding based on screen height
+                  child: Center(
+                    child: Container(
+                      width: ScreenUtil().setWidth(
+                          200), // Adjust the width based on screen width
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(ScreenUtil().setWidth(10)),
+                        color: gridHomeColor,
                       ),
-                      label: const Text(
-                        'home',
-                        style: TextStyle(color: faceBookColor),
+                      child: TextButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const RegisteredHomeScreen()),
+                          );
+                        },
+                        icon: const Icon(
+                          Icons.home,
+                          color: faceBookColor,
+                        ),
+                        label: const Text(
+                          'home',
+                          style: TextStyle(color: faceBookColor),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
