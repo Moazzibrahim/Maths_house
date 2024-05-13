cd4bc49cd6509dc50bc487143743172841ea8f52
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-05-13 11:07:22 +0300

    wa

---
+++ lib/Model/wallet_history_model.dart
@@ -0,0 +1,42 @@
+class WalletHistory {
+  final int studentId;
+  final int wallet;
+  final String date;
+  final String state;
+
+  WalletHistory({
+    required this.studentId,
+    required this.wallet,
+    required this.date,
+    required this.state,
+  });
+
+  factory WalletHistory.fromJson(Map<String, dynamic> json) {
+    return WalletHistory(
+      studentId: json['student_id'],
+      wallet: json['wallet'],
+      date: json['date'],
+      state: json['state'],
+    );
+  }
+}
+
+class WalletHistoryList {
+  final int totalWallet;
+  final List<WalletHistory> walletHistoryList;
+
+  WalletHistoryList({
+    required this.totalWallet,
+    required this.walletHistoryList,
+  });
+
+  factory WalletHistoryList.fromJson(Map<String, dynamic> json) {
+    var list = json['walletHestory'] as List;
+    List<WalletHistory> walletHistoryList =
+        list.map((e) => WalletHistory.fromJson(e)).toList();
+    return WalletHistoryList(
+      totalWallet: int.parse(json['totalWallet']),
+      walletHistoryList: walletHistoryList,
+    );
+  }
+}
