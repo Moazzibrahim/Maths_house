cb65e837ed9ae2a89b350df2c3b26f5bf4a79f03
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-07-21 16:33:12 +0300

    video live

--- lib/View/screens/live/video_live.dart
+++ lib/View/screens/live/video_live.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/View/screens/live/my_live_Sceen.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class VideoWebView extends StatefulWidget {
@@ -12,43 +12,119 @@ class VideoWebView extends StatefulWidget {
 }
 
 class _VideoWebViewState extends State<VideoWebView> {
+  bool isLandscape = false;
   final controller = WebViewController()
     ..setJavaScriptMode(JavaScriptMode.unrestricted)
     ..loadRequest(Uri.parse(
         "https://ucloud.mfscripts.com/video/embed/4g/640x320/Iron_Sky_Trailer.mp4"));
-  @override
-  void initState() {
-    super.initState();
 
-    // SystemChrome.setPreferredOrientations([
-    //   // DeviceOrientation.landscapeRight,
-    //   // DeviceOrientation.landscapeLeft,
-    // ]);
-  }
-
-  @override
-  void dispose() {
-    // SystemChrome.setPreferredOrientations([
-    //   DeviceOrientation.portraitUp,
-    //   DeviceOrientation.portraitDown,
-    // ]);
-    super.dispose();
+  void toggleRotation() {
+    if (isLandscape) {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
+    } else {
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeRight,
+        DeviceOrientation.landscapeLeft,
+      ]);
+    }
+    setState(() {
+      isLandscape = !isLandscape;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Embedded Video'),
+        title: const Text('Ideas Content'),
+        actions: [
+          IconButton(
+            icon: Icon(isLandscape
+                ? Icons.screen_lock_rotation
+                : Icons.screen_rotation),
+            onPressed: toggleRotation,
+          ),
+        ],
       ),
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: 16 / 9,
-          child: WebViewWidget(
-            controller: controller,
-            // initialUrl: widget.url, // Use the URL from the widget
-            // javascriptMode: JavascriptMode.unrestricted,
-            // initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AspectRatio(
+                aspectRatio: 21 / 9,
+                child: WebViewWidget(
+                  controller: controller,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Chapter 1: Lesson 1',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/logo.png'),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Mr. Amir',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  for (int i = 0; i < 5; i++)
+                    GestureDetector(
+                      onTap: () {
+                        // Add rating update logic here if needed
+                      },
+                      child: Icon(
+                        i < 3
+                            ? Icons.star
+                            : Icons
+                                .star_border_outlined, // Adjust rating condition as needed
+                        color: Colors.redAccent[700],
+                      ),
+                    ),
+                  const SizedBox(width: 10),
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.shade300,
+                    ),
+                    child: const Row(
+                      children: [
+                        Text('Report'),
+                        SizedBox(width: 5),
+                        Icon(Icons.flag_outlined),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'No more videos',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ],
           ),
         ),
       ),
@@ -57,7 +133,6 @@ class _VideoWebViewState extends State<VideoWebView> {
 }
 
 void main() {
-  // WebView.platform;
   runApp(const MyApp());
 }
 
@@ -71,7 +146,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyLiveScreen(),
+      home: const VideoWebView(
+          url:
+              'https://ucloud.mfscripts.com/video/embed/4g/640x320/Iron_Sky_Trailer.mp4'),
     );
   }
 }
