f66c0c260c5399992d0121ab485c439c9b340492
Ahmed-Ibrahim618 <ahmed.ibrahim61879@gmail.com>
2024-08-12 16:26:37 +0300

    exam duration

--- lib/View/screens/exam-view/exam_duration.dart
+++ lib/View/screens/exam-view/exam_duration.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/View/screens/checkout/checkout_chapter_screen.dart';
-import 'package:flutter_application_1/View/screens/tabs_screen.dart';
-import 'package:flutter_application_1/constants/colors.dart';
+import 'package:flutter_application_1/constants/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ExamDuration extends StatefulWidget {
@@ -39,6 +38,9 @@ class _ExamDurationState extends State<ExamDuration> {
       setState(() {
         showError = true;
       });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('You must select at least one duration!')),
+      );
     } else {
       setState(() {
         showError = false;
@@ -55,75 +57,93 @@ class _ExamDurationState extends State<ExamDuration> {
 
   @override
   Widget build(BuildContext context) {
-    // Create a unique list of durations
     List<int> uniqueDurations = widget.durations.toSet().toList();
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Exam Duration"),
-        leading: Container(
-          margin: const EdgeInsets.all(6),
-          decoration: BoxDecoration(
-              color: gridHomeColor, borderRadius: BorderRadius.circular(12)),
-          child: InkWell(
-            child: const Icon(
-              Icons.arrow_back,
-              color: faceBookColor,
-            ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                    builder: (context) => const TabsScreen(
-                          isLoggedIn: false,
-                        )),
-              );
-            },
-          ),
-        ),
-      ),
+      appBar: buildAppBar(context, 'Exam Duration'),
       body: Padding(
-        padding: EdgeInsets.all(8.w),
+        padding: EdgeInsets.all(12.w),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              for (var i = 0; i < widget.chapterNames.length; i++)
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(12.w),
+                margin: EdgeInsets.only(bottom: 16.h),
+                decoration: BoxDecoration(
+                  color: Colors.red[100], // Light yellow background
+                  borderRadius: BorderRadius.circular(8), // Rounded corners
+                  border: Border.all(color: Colors.red[700]!, width: 1.5),
+                ),
+                child: Row(
                   children: [
-                    Text(
-                      'Select duration for: ${widget.chapterNames[i]}',
-                      style: TextStyle(fontSize: 16.sp),
+                    Icon(
+                      Icons.info_outline,
+                      color: Colors.red[700],
                     ),
-                    DropdownButton<int>(
-                      hint: const Text("Select Duration"),
-                      items: uniqueDurations
-                          .map(
-                            (duration) => DropdownMenuItem<int>(
-                              value: duration,
-                              child: Text('$duration '),
-                            ),
-                          )
-                          .toList(),
-                      onChanged: (selectedDuration) {
-                        if (selectedDuration != null) {
-                          setState(() {
-                            int index =
-                                widget.durations.indexOf(selectedDuration);
-                            double price = widget.prices[index];
-                            selectedPrices[i] = price;
-                            selectedDurations[i] = selectedDuration;
-                            _updateTotalPrice();
-                          });
-                        }
-                      },
-                    ),
-                    if (selectedPrices.containsKey(i))
-                      Text(
-                        'Price: \$${selectedPrices[i]!.toStringAsFixed(2)}',
-                        style: TextStyle(fontSize: 14.sp),
+                    SizedBox(width: 8.w),
+                    Expanded(
+                      child: Text(
+                        'You should select a duration for needed chapters only.',
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red[900], // Darker yellow text
+                        ),
                       ),
-                    SizedBox(height: 16.h),
+                    ),
                   ],
+                ),
+              ),
+              for (var i = 0; i < widget.chapterNames.length; i++)
+                Card(
+                  margin: EdgeInsets.only(bottom: 16.h),
+                  elevation: 3,
+                  child: Padding(
+                    padding: EdgeInsets.all(12.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Select duration for: ${widget.chapterNames[i]}',
+                          style: TextStyle(fontSize: 16.sp),
+                        ),
+                        DropdownButton<int>(
+                          hint: const Text("Select Duration"),
+                          items: uniqueDurations
+                              .map(
+                                (duration) => DropdownMenuItem<int>(
+                                  value: duration,
+                                  child: Text('$duration '),
+                                ),
+                              )
+                              .toList(),
+                          onChanged: (selectedDuration) {
+                            if (selectedDuration != null) {
+                              setState(() {
+                                int index =
+                                    widget.durations.indexOf(selectedDuration);
+                                double price = widget.prices[index];
+                                selectedPrices[i] = price;
+                                selectedDurations[i] = selectedDuration;
+                                _updateTotalPrice();
+                              });
+                            }
+                          },
+                        ),
+                        if (selectedPrices.containsKey(i))
+                          Text(
+                            'Price: \$${selectedPrices[i]!.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        SizedBox(height: 8.h),
+                      ],
+                    ),
+                  ),
                 ),
               const Divider(),
               Text(
@@ -133,24 +153,20 @@ class _ExamDurationState extends State<ExamDuration> {
               const SizedBox(
                 height: 14,
               ),
-              if (showError)
-                Text(
-                  'You must select at least one duration first!',
-                  style: TextStyle(fontSize: 14.sp, color: Colors.red),
-                ),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
+              ElevatedButton.icon(
                 style: ElevatedButton.styleFrom(
-                  shape: const RoundedRectangleBorder(),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 12.h, horizontal: 20.w),
                   backgroundColor: Colors.redAccent[700],
-                  foregroundColor: Colors.white,
                 ),
                 onPressed: _checkout,
-                child: const Text(
-                  "check out",
-                  style: TextStyle(fontSize: 13, color: Colors.white),
+                icon: const Icon(Icons.shopping_cart),
+                label: const Text(
+                  "Check Out",
+                  style: TextStyle(fontSize: 15, color: Colors.white),
                 ),
               ),
             ],
